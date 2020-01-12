12406164da1bd9d0407d2a6631a381c70e4a382e
Smeunzo <fsl12@hotmail.fr>
2020-01-12 07:48:44 +0100

    feat:Gallery_test
    test n°2 : Fichier vide / non existant

--- tests/models/pictures/PictureModelImpl.test.ts
+++ tests/models/pictures/PictureModelImpl.test.ts
@@ -1,55 +1,72 @@
 import {expect} from "chai";
 import {PictureModelImpl} from "../../../src/models/picture/PictureModelImpl";
-import {Db, MongoClient} from "mongodb";
+import {Db, MongoClient, ObjectID} from "mongodb";
 import {Picture} from "../../../src/models/picture/Picture";
 import {Request, request} from "express";
 import {User} from "../../../src/models/auth/User";
 
-describe('PictureModelImpl',() =>{
+describe('PictureModelImpl', () => {
 
-    let pictureModel : PictureModelImpl;
-    let mongoClient : MongoClient;
-    let db : Db;
-    let file : Request["file"];
-    let picture : Picture;
-    let user: User;
+    let pictureModel: PictureModelImpl;
+    let mongoClient: MongoClient;
+    let db: Db;
 
-    before(async () =>{
-        mongoClient = await MongoClient.connect('mongodb://localhost', {useUnifiedTopology : true});
+    let picture: Picture;
+
+
+    before(async () => {
+        mongoClient = await MongoClient.connect('mongodb://localhost', {useUnifiedTopology: true});
         db = mongoClient.db('test');
         pictureModel = new PictureModelImpl(db);
     });
 
-    describe('#uploadFile', async () =>{
-        file = {
-            fieldname:"image",
-            originalname:"image.png",
-            encoding:"7bit",
+    describe('#uploadFile', async () => {
+        const fakeUser: User = {_id: new ObjectID(), username: "test"};
+        let emptyFile: Request['file'];
+
+        let emptyUser: User;
+        let fakeFile: Request["file"] = {
+            fieldname: "image",
+            originalname: "image.png",
+            encoding: "7bit",
             mimetype: "image/png",
             size: 9000,
-            destination:"./public/pictures",
-            filename:"image.png",
-            path:"/public/pictures/image.png",
-            buffer: Buffer.from(new ArrayBuffer(0),0,0),
-            location:""
+            destination: "./public/pictures",
+            filename: "image.png",
+            path: "/public/pictures/image.png",
+            buffer: Buffer.from(new ArrayBuffer(0), 0, 0),
+            location: ""
         };
 
-        it("should throw 'l\'utilisateur n\'est pas connecté' ",async ()=>{
-           try{
-                await pictureModel.uploadFile(file,user);
-           }catch (errors) {
-               expect(errors.message).to.be.equals('L\'utilisateur n\'est pas connecté');
-               return ;
-           }
-           expect.fail();
-        })
+        it("should throw \"Une erreur avec le fichier\"", async () => {
+            try {
+                await pictureModel.uploadFile(emptyFile, fakeUser);
+            } catch (errors) {
+                expect(errors.message).to.be.equals("Une erreur avec le fichier");
+                return;
+            }
+            expect.fail()
+        });
+
+
+        it("should throw \"l'utilisateur n'est pas connecté\" ", async () => {
+            try {
+                await pictureModel.uploadFile(fakeFile, emptyUser);
+            } catch (errors) {
+                expect(errors.message).to.be.equals('L\'utilisateur n\'est pas connecté');
+                return;
+            }
+            expect.fail();
+        });
+
+
     });
 
-    afterEach( async () =>{
+    afterEach(async () => {
         await db.dropDatabase();
     });
 
-    after(async  () =>{
+    after(async () => {
         await mongoClient.close()
     });
 });
